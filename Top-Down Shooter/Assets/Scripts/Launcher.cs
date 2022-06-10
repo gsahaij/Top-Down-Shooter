3e227e6b9eb3859358f2e0f113a768c6ac371e5c
QuinnJohnsonCode <Quinn.Johnson@hotmail.ca>
2022-06-10 17:47:14 -0400

    Fixed Pivot
    - Pivots around the player and shoots in the correct direction now
    - Sourced: https://answers.unity.com/questions/794119/how-to-rotate-an-object-around-a-fixed-point-so-it.html

--- Top-Down Shooter/Assets/Scripts/Launcher.cs
+++ Top-Down Shooter/Assets/Scripts/Launcher.cs
@@ -8,18 +8,39 @@ public class Launcher : MonoBehaviour
     public Transform firePoint;
     public float fireForce = 20f;
     
-    [SerializeField]
-    Rigidbody2D rb;
+
+
+    public Transform centerPoint;
+    private Vector2 v;
+
 
     MouseController ms = new MouseController();
-    public void FixedUpdate()
+
+    private void Start()
     {
-        rb.rotation = ms.GetMouseAngle(rb);
+        v = (transform.position - centerPoint.position);
+    }
+
+    private void Update()
+    {
+        // Source: https://answers.unity.com/questions/794119/how-to-rotate-an-object-around-a-fixed-point-so-it.html
+        Vector3 centerScreenPos = Camera.main.WorldToScreenPoint(centerPoint.position);
+        Vector2 dir = Input.mousePosition - centerScreenPos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.position = centerPoint.position + q * v;
+        transform.rotation = q;
+        //firePoint.rotation = q;
+        
+
+
+
+        //rb.rotation = ms.GetMouseAngle(rb);
     }
     public void Fire()
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.right * fireForce, ForceMode2D.Impulse);
     }
 
 }
