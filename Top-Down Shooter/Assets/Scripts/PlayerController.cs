31c92ebb04a2ab5821abff16ed36ba16cbfa0075
QuinnJohnsonCode <Quinn.Johnson@hotmail.ca>
2022-06-12 11:24:24 -0400

    Script Cleanup
    - Added comments to three scripts
    - Cleaned and took out redundant code
    - Added methods where logical

--- Top-Down Shooter/Assets/Scripts/PlayerController.cs
+++ Top-Down Shooter/Assets/Scripts/PlayerController.cs
@@ -4,28 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
-    public Launcher rLauncher;
+    // Public Fields
+    public float moveSpeed = 5f;            // Determines how fast the player moves
+    public Rigidbody2D rb;                  // Holds Player's rigidbody
+    public Launcher rLauncher;              // Reference to the Player's rocket launcher
 
-    Vector2 moveDirection;
+    // Private Fields
+    Vector2 moveDirection;                  // Holds x and y for directional movements
 
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-
-        if(Input.GetMouseButtonDown(0))
-        {
-            rLauncher.Fire();
-        }
-
-        moveDirection = new Vector2(moveX, moveY).normalized;
+        CalculateMovement();
+        CheckFire();
     }
 
     void FixedUpdate()
     {
+        Move();
+    }
+
+    /* Checks the H/V axis and creates a new Vector2 based on them (moveDirection) */
+    void CalculateMovement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    /* Changes the player's velocity based on the moveDirection and moveSpeed */
+    void Move()
+    {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    /* Checks if the left mouse button is held, then calls fire on the launcher */
+    void CheckFire()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rLauncher.Fire();
+        }
     }
 }
