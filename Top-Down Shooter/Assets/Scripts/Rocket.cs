47298c67f1f30d27db89a87e508022f8440ea1d8
QuinnJohnsonCode <Quinn.Johnson@hotmail.ca>
2022-06-24 19:50:47 -0400

    Added Explosion Effect
    - Two Particle Systems to create an explosion effect when the rocket comes in contact with an enemy

--- Top-Down Shooter/Assets/Scripts/Rocket.cs
+++ Top-Down Shooter/Assets/Scripts/Rocket.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    public GameObject explosionEffectPrefab;            // Holds the prefab for the explosion effect
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Instantiate(explosionEffectPrefab, collision.gameObject.transform.position, Quaternion.identity);
+            Destroy(collision.gameObject);
+        }
         Destroy(gameObject);
-        // Check if you're hitting the enemy here
     }
 }
